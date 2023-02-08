e45181166436dc5f436a97db8c90c3a0238595eb
yuusheng <hi@yuusheng.im>
2023-02-08 13:44:02 +0800

    lint: fix

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js"
+import { createSignal } from 'solid-js'
 
 export default function Counter() {
   const [count, setCount] = createSignal(0)
@@ -9,6 +9,6 @@ export default function Counter() {
       onClick={() => setCount(count() + 1)}
     >
       Count {count()}
-    </button>  
+    </button>
   </div>
 }
